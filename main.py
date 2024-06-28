eceb539782c4577ee8399918808e70a82c65874c
lcourbi <luc.courbariaux@gmail.com>
2024-06-28 20:06:35 +0200

    refactor main.py script, update addon README

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main():
 	commandLineArgs.add_argument('-t', '--transpiler', nargs = '?', default = 'CSharp', help='which transpiler script to use')
 	commandLineArgs.add_argument('-v', '--verbose', action='store_true', default = False, help='print additional execution logs' )
 	commandLineArgs.add_argument('--use_floats', action='store_true', default = False, help='leave floating point types as floats' )
-	commandLineArgs.add_argument('--verboseP', action='store_true', default = False, help='print additional parser execution logs' )
-	commandLineArgs.add_argument('--verboseT', action='store_true', default = False, help='print additional transpiler execution logs' )
+	commandLineArgs.add_argument('--transpiler_verbose', action='store_true', default = False, help='print additional parser execution logs' )
+	commandLineArgs.add_argument('--parser_verbose', action='store_true', default = False, help='print additional transpiler execution logs' )
 	commandLineArgs.add_argument('--no_type_resolving', action='store_true', default = False, help='removes the initial type resolving step for user types' )
 	commandLineArgs.add_argument('--no_save', action='store_true', default = False, help='do not save output code as a file' )
-	commandLineArgs.add_argument('--tokens', action='store_true', default = False, help='if set will print the tokenizer output' )
+	commandLineArgs.add_argument('--print_tokens', action='store_true', default = False, help='if set will print the tokenizer output' )
 	args = commandLineArgs.parse_args()
 	
 	import src
@@ -23,13 +23,15 @@ def main():
 
 	# dynamic import
 	Transpiler = __import__(args.transpiler.replace('.py', ''))
+	Transpiler.use_floats = args.use_floats
 
 	# for verbose printing
 	def getPrinter(condition): return print if condition else lambda a,*b:None
 
 	# files to transpile
 	input_files = set()
-	for path in args.input:
+	args.output = os.path.normpath(args.output)
+	for path in map(os.path.normpath, args.input):
 		if os.path.isdir(path):
 			# Find all gd files in input dir
 			input_files.update([
@@ -42,64 +44,51 @@ def main():
 			# simple file
 			input_files.add( path )
 
-
 	if args.verbose:
 		print(f"args: {sys.argv}")
 		print(f"files to process :\n{input_files}")
 
 	total = len(input_files)
 
-	script_classes = {}
 
 	# script name without extension
 	to_script_name = lambda s: os.path.basename(s).split('.')[0]
+	# dir/script name
+	to_simple_path = lambda s: f'{s.split(os.path.sep)[-2]}/{os.path.basename(s)}'
 
 	# type resolving step : useful for both calling user classes from another
 	# and for using method result type before it is defined  
+	script_classes = {}
 	if not args.no_type_resolving:
 		for i, filename in enumerate(input_files):
-
-			with open(filename,'r+') as f:
-				text = f.read()
-
-			# script name without extension
-			script_name = to_script_name(filename)
-
-			transpiler = TypeResolver()
-			parser = Parser.Parser(script_name, text, transpiler, lambda a,*b:None )
-
 			try:
+				with open(filename,'r+') as f: text = f.read()
+				parser = Parser.Parser(to_script_name(filename), text, TypeResolver(), lambda a,*b:None )
 				parser.transpile()
 				script_classes[parser.getClassName()] = parser.getClass()
-				#print('done', filename, script_classes.keys())
 				
 			except Exception as e:
 				handleException(e)
 
+		# we add the dduces types the parser class,
+		# they'll be available in the actual trnaspiling step
 		Parser.godot_types.update(script_classes)
 
 	for i, filename in enumerate(input_files):
-		
-		outname = filename.replace(os.path.dirname(filename), args.output).replace('.gd', '')
-		os.makedirs(os.path.dirname(outname), exist_ok=True)
-		
-		with open(filename,'r+') as f:
-			text = f.read()
-		
-		script_name = to_script_name(filename)
-		
-		Transpiler.use_floats = args.use_floats
-		transpiler = Transpiler.Transpiler(script_name, outname, getPrinter(args.verbose or args.verboseT) )
-		
-		parser = Parser.Parser(script_name, text, transpiler, getPrinter(args.verbose or args.verboseP) )
-		
-		if args.tokens:
-			print(outname)
-			print('\n'.join(map(lambda token: f'line {token.lineno}: {token.type} <{token.value}>', parser.tokenizer.tokenize(text))))
-		
-		printException = lambda : None
-		
 		try:
+		
+			outname = filename.replace(os.path.dirname(filename), args.output).replace('.gd', '')
+			os.makedirs(os.path.dirname(outname), exist_ok=True)
+			
+			with open(filename,'r+') as f: text = f.read()
+			
+			script_name = to_script_name(filename)
+			transpiler = Transpiler.Transpiler(script_name, outname, getPrinter(args.verbose or args.transpiler_verbose) )
+			parser = Parser.Parser(script_name, text, transpiler, getPrinter(args.verbose or args.parser_verbose) )
+			
+			if args.print_tokens:
+				print('\n'.join(map(lambda token: f'line {token.lineno}: {token.type} <{token.value}>', parser.tokenizer.tokenize(text))))
+		
 			parser.transpile()
 			
 		except Exception as e:
@@ -107,8 +96,8 @@ def main():
 			
 		if not args.no_save:
 			transpiler.save_result()
-		
-		print(f"Converted {filename} to {outname} ({i+1}/{total})")
+
+		print(f"Converted {to_simple_path(filename)} to {to_simple_path(outname)} ({i+1}/{total})")
 
 def handleException(e):
 		ex_msg = str(e); ex_msg = ex_msg if ex_msg != 'None' else ''
