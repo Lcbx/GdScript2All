4aa5beb045fc641d94fc02b9b7d15ff7b02eccfa
Lcbx <luc.courbariaux@gmail.com>
2024-09-25 17:55:34 +0300

    experiment with generating a godot-cpp project based on official godot template

--- addons/gdscript2all/converter/main.py
+++ addons/gdscript2all/converter/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from subprocess import run
 
 def main():
 	
@@ -15,7 +16,8 @@ def main():
 	commandLineArgs.add_argument('--no_type_resolving', action='store_true', default = False, help='removes the initial type resolving step for user types' )
 	commandLineArgs.add_argument('--no_save', action='store_true', default = False, help='do not save output code as a file' )
 	commandLineArgs.add_argument('--print_tokens', action='store_true', default = False, help='print the tokenizer output' )
-	commandLineArgs.add_argument('--log_file', default = None, help='redirect stdout and stderr to specified filepath' )
+	commandLineArgs.add_argument('--log_file', default = '', help='redirect stdout and stderr to specified filepath' )
+	commandLineArgs.add_argument('--create_gdextension', default = '', help='creates a gdextension cpp project in the output dir with specified name' )
 	args = commandLineArgs.parse_args()
 	
 	import src
@@ -73,14 +75,19 @@ def main():
 			except Exception as e:
 				handleException(e)
 
-		# we add the dduces types the parser class,
-		# they'll be available in the actual trnaspiling step
+		# we add the deduced types the parser class,
+		# they'll be available in the actual transpiling step
 		Parser.godot_types.update(script_classes)
+
+	# generate the cpp project if specified
+	if project_name := args.create_gdextension:
+		generate_project(args.output, project_name, script_classes.keys())
 
 	for i, filename in enumerate(input_files):
 		try:
-		
-			outname = filename.replace(os.path.dirname(filename), args.output).replace('.gd', '')
+			
+			filedir = os.path.dirname(filename)
+			outname = (filename.replace(filedir, args.output) if filedir else os.path.join(args.output, filename) ).replace('.gd', '')
 			os.makedirs(os.path.dirname(outname), exist_ok=True)
 			
 			with open(filename,'r+') as f: text = f.read()
@@ -102,6 +109,7 @@ def main():
 
 		print(f"Converted {to_simple_path(filename)} to {to_simple_path(outname)} ({i+1}/{total})")
 
+
 def handleException(e):
 		ex_msg = str(e); ex_msg = ex_msg if ex_msg != 'None' else ''
 		ex_type = type(e).__name__
@@ -114,6 +122,55 @@ def handleException(e):
 			name = tb.tb_frame.f_code.co_name
 			print(f'  at {filename}:{lineno}\t{name}')
 			tb = tb.tb_next
+
+def generate_project(base_path, project_name : str, classes : list) -> None:
+	project_path = os.path.join(base_path, project_name)
+
+	# official template
+	TEMPLATE_URL = 'https://github.com/godotengine/godot-cpp-template'
+
+	# dowload the official template project
+	run(['git', 'clone', f'{TEMPLATE_URL}.git', project_path])
+
+	# go into created project dir
+	os.chdir(project_path)
+
+	# setup godot-cpp
+	run(['git', 'submodule', 'update', '--init'])
+
+	# Overwrite the old main branch reference with the new one
+	run(['git', 'checkout', '--orphan', 'new-main', 'main'])
+	run(['git', 'commit', '-m', f'"initial commit based on {TEMPLATE_URL}"'])
+	run(['git', 'branch', '-M', 'new-main', 'main'])
+
+	# NOTE: following manipulations need to be kept up to date
+	# with template's dir structure and file contents
+
+	init = { 'example_library_init':f'{project_name}_init' }
+	register_classes = { '//ClassDB::register_class<YourClass>();':
+		'\n\t'.join(map(lambda c: f'ClassDB::register_class<{c}>();', classes))
+	}
+	file_replace('SConstruct', {'EXTENSION-NAME':project_name,
+		# temporary fix
+		'env.InstallAs("{}/bin/{}/lib{}".format(projectdir, env["platform"], file), library)':'env.InstallAs("{}/bin/lib{}".format(projectdir, file), library)'
+	})
+	file_replace('src/register_types.h', {'EXAMPLE_REGISTER_TYPES_H':f'{project_name.upper()}_REGISTER_TYPES_H'})
+	file_replace('src/register_types.cpp', {**init, **register_classes})
+	file_replace('demo/bin/example.gdextension', {**init, 'gdexample':project_name})
+	os.rename('demo/bin/example.gdextension', f'demo/bin/{project_name}.gdextension')
+	
+	# go back
+	os.chdir('../..')
+
+def file_replace(file_name, changes_dict : dict):
+    with open(file_name, "r+") as file:
+        contents = file.read()
+        for old, new in changes_dict.items():
+        	contents = contents.replace(old, new)
+
+        file.seek(0)
+        file.write(contents)
+        file.truncate()
 
 if __name__ == '__main__':
 	main()
--- generate_addon.sh
+++ generate_addon.sh
@@ -1,4 +1,4 @@
-py -m nuitka --standalone --show-progress addons/gdscript2all/converter/main.py --output-dir=nuitka_build --include-plugin-directory=addons/gdscript2all/converter/libs --include-package=src
+python -m nuitka --standalone --show-progress addons/gdscript2all/converter/main.py --output-dir=nuitka_build --include-plugin-directory=addons/gdscript2all/converter/libs --include-package=src
 
 mv -f nuitka_build/main.dist/* addons/gdscript2all/converter/build
 
